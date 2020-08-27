a094fc3df400a1ff95da7d4fcb5b740b22f0a2ef
MattiaG-afk <53443116+MattiaG-afk@users.noreply.github.com>
2020-08-27 21:47:15 +0200

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,17 @@
+import subprocess
+try:
+    print("    _         _                        _            ")
+    print("   / \  _   _| |_ ___  _ __ ___   __ _| |_ ___ _ __ ")
+    print("  / _ \| | | | __/ _ \| '_ ` _ \ / _` | __/ _ \ '__|")
+    print(" / ___ \ |_| | || (_) | | | | | | (_| | ||  __/ |   ")
+    print("/_/   \_\__,_|\__\___/|_| |_| |_|\__,_|\__\___|_|   By @Kr0n0s")
+    print()
+    print("Installer for OS that have apt for package manager")
+    print("[+] Updating repository...")
+    subprocess.run(["apt update"])
+    print("[+] Installing aircrack-ng...")
+    subprocess.run(["apt install -y aircrack-ng"])
+    print("[+] Installing nikto...")
+    subprocess.run(["apt install -y nikto"])
+except:
+    print("There is some error... Quitting")
