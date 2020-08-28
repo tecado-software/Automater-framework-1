266e20d972b2989310b4aed3b8a0313ed56170b1
Mattia <53443116+MattiaG-afk@users.noreply.github.com>
2020-08-28 12:13:46 +0200

    Fixed some bugs

--- installer.py
+++ installer.py
@@ -8,10 +8,10 @@ try:
     print()
     print("Installer for OS that have apt for package manager")
     print("[+] Updating repository...")
-    subprocess.run(["apt update"])
+    subprocess.run("apt update", shell=True)
     print("[+] Installing aircrack-ng...")
-    subprocess.run(["apt install -y aircrack-ng"])
+    subprocess.run("apt install -y aircrack-ng", shell=True)
     print("[+] Installing nikto...")
-    subprocess.run(["apt install -y nikto"])
+    subprocess.run("apt install -y nikto", shell=True)
 except:
     print("There is some error... Quitting")
